21f8391e9affa4b7b6a8cbe60ebfab060d0352a2
AJ197028 <aksjais20@gmail.com>
2024-03-22 04:21:20 +0530

    cart +/- functionality

--- src/pages/cart/cart-item.js
+++ src/pages/cart/cart-item.js
@@ -1,27 +1,27 @@
-import React, { useContext } from 'react';
-import "./cart.css"
-import { ShopContext } from '../../context/shop-context';
+import React, { useContext } from "react";
+import "./cart.css";
+import { ShopContext } from "../../context/shop-context";
 
 const CartItem = (props) => {
-    const { id,productName, price, productImage } = props.data;
-    const {cartItems} = useContext(ShopContext);
+  const { id, productName, price, productImage } = props.data;
+  const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
 
   return (
-    <div className='cartItem'>
-        <img src={productImage}/>
-        <div className='description'>
-            <p>
-                <b> {productName} </b>
-            </p>
-            <p> ${price} </p>
-            <div className='countHandler'>
-                <button>+</button>
-                <input value={cartItems[id]}/>
-                <button>-</button>
-            </div>
+    <div className="cartItem">
+      <img src={productImage} />
+      <div className="description">
+        <p>
+          <b> {productName} </b>
+        </p>
+        <p> ${price} </p>
+        <div className="countHandler">
+          <button onClick={()=>removeFromCart(id)}>-</button>
+          <input value={cartItems[id]} />
+          <button onClick={()=>addToCart(id)}>+</button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
